46dca091c9712438d26be85f61121d7328555001
dashodanger <none@none.com>
2022-05-25 08:18:12 -0600

    Cleaned up previous in-file licensing statements that were under previous license

--- externs.h
+++ externs.h
@@ -1,15 +1,6 @@
 /* File: externs.h */
 
 /*
- * Copyright (c) 1997 Ben Harrison
- *
- * This software may be copied and distributed for educational, research,
- * and not for profit purposes provided that this copyright and statement
- * are included in all such copies.
- */
-
-
-/*
  * Note that some files have their own header files
  * (z-virt.h, z-util.h, z-form.h, term.h, random.h)
  */
